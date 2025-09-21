2c074e96b2d7f188f62832b024722a5278659b64
yauheniya <evgeniya.boroznovskaya@gmail.com>
2025-09-21 17:35:47 +0300

    maven is enabled

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        String  myInitialString = "My name is Broccoli.";
+        System.out.println(myInitialString);
+        processString(myInitialString);
+
+        NewClass initialString = new NewClass();
+        initialString.str = "My initial value";
+        processNewClass(initialString);
+        System.out.println(initialString);
     }
+    public static void processString(String string){
+        string += " Hello!!!!!!!!!!";
+        System.out.println(string);
+    }
+
+    public static void processNewClass(NewClass string){
+        string.str += " bububu. I've been changed.";
+    }
+
 }
